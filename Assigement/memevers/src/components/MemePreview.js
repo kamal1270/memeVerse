542196f84760fe4e5a3c97a3fc9fee19d4d9a9fc
kamal1270 <113257103+kamal1270@users.noreply.github.com>
2025-03-07 01:03:29 +0530

    Delete Assigement directory

--- Assigement/memevers/src/components/MemePreview.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MemePreview = ({ image }) => {
-    return <div>{image && <img src={image} alt="Meme preview" style={{ maxWidth: "300px" }} />}</div>;
-};
-
-export default MemePreview;
--- Assigement/memevers/src/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    darkMode: "class", // Enables dark mode support
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
